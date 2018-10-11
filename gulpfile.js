36e973ae4048702b91811c5c77c962de557705a3
Unknown <muffinking3@gmail.com>
2018-10-11 02:17:15 -0400

    update

--- gulpfile.js
+++ gulpfile.js
@@ -102,4 +102,5 @@ gulp.task("test", function (done) {
 gulp.task("build", function(done) { gulp.series("copy", "html", "lint", "test")(); done(); });
 gulp.task("development", function(done) { gulp.series("watch", "copy", "html", "lint", "test", "server")(); done(); });
 gulp.task("production", function(done) { gulp.series("copy", "scripts", "html")(); done(); });
-gulp.task("default",  function(done) { gulp.series(config.env)(); done(); });
+gulp.task("default", function(done) { gulp.series("copy", "scripts", "html")(); done(); });
+// gulp.task("default",  function(done) { gulp.series(config.env)(); done(); });
