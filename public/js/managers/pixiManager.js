3cb4153ae13d15a6c29fb3ba50990ece4c03a973
daniel.xiao <daniel.xiao@chromeriver.com>
2018-11-13 22:26:29 -0800

    move the boring stage draw stuff to a separate file

--- public/js/managers/pixiManager.js
+++ public/js/managers/pixiManager.js
@@ -5,16 +5,15 @@ import gameState from 'data/gameState';
 import Player from 'components/Player';
 import Ball from 'components/Ball';
 import SocketClient from 'components/SocketClient';
-import TextComponent from 'components/TextComponent';
 
-import { DASH_SIZE, GAME_SIZE, PADDLE_SIZE } from 'constants/sizes';
-import { BALL_DEFAULT_POS, PRIMARY_PLAYER_DEFAULT_POS, SECONDARY_PLAYER_DEFAULT_POS } from 'constants/positions';
+import { GAME_SIZE } from 'constants/sizes';
 
 import { getCanvasContainer } from 'helpers/canvasHelper';
+import { drawField, drawScores } from 'helpers/pixiGameDrawHelper';
 
 /*
   singleton for Pixi.js
-    use this to do generic stuff
+    use this to draw and update the view
 */
 // set up Application
 const app = new PIXI.Application(GAME_SIZE);
@@ -41,8 +40,8 @@ const setupApp = () => {
 
   // -- render starts here
   // draw the field and score
-  drawField();
-  drawScores();
+  drawField(stage);
+  drawScores(stage);
 
   // opposing player
   stage.addChild(secondaryPlayer.view);
@@ -53,9 +52,12 @@ const setupApp = () => {
   // ball
   stage.addChild(ball.view);
 
+  // after adding all the components, finally we can update
   appInitUpdate();
 };
-
+/**
+ * add a constant ticker to update the game
+ */
 const appInitUpdate = () => {
   app.ticker.add(function(delta) {
     // call updates of each object
@@ -73,71 +75,6 @@ const appInitUpdate = () => {
     secondaryPlayer.view.position.x = secondaryPlayer.position.x;
   });
 }
-/**
- * draw some graphics for the playing field
- */
-const drawField = () => {
-  const stage = app.stage;
-  const fieldGraphics = new PIXI.Graphics();
-
-  const { width, height } = DASH_SIZE;
-  const dashVerticalOffset = (PADDLE_SIZE.height / 2) + 15;
-  const dashCount = (GAME_SIZE.width / width) / 2;
-
-  for (let i = 0; i < dashCount; i ++) {
-    const dashDistance = (width * 2) * i + (width / 2);
-    // upper line
-    fieldGraphics.beginFill(0x333333);
-    fieldGraphics.drawRect(dashDistance, SECONDARY_PLAYER_DEFAULT_POS.y - dashVerticalOffset, width, height);
-
-    // lower line
-    fieldGraphics.drawRect(dashDistance, PRIMARY_PLAYER_DEFAULT_POS.y + dashVerticalOffset, width, height);
-
-    // center line
-    fieldGraphics.beginFill(0x6d6d6d);
-    fieldGraphics.drawRect(dashDistance, app.screen.height / 2, width, height);
-  };
-
-  fieldGraphics.endFill();
-  stage.addChild(fieldGraphics);
-};
-/**
- * draw the Scores
- */
-const drawScores = () => {
-  const stage = app.stage;
-  const scoreStyles = {
-    fill: 0x6d6d6d,
-    fontSize: 36,
-    fontWeight: 'bold',
-  };
-
-  const fieldVerticalCenter = app.screen.height / 2;
-  const scoreOffset = {
-    x: 35,
-    y: 45,
-  };
-
-  // secondary player, score is above
-  const secondaryPlayerScore = new TextComponent('0', {
-    ...scoreStyles,
-    position: {
-      x: scoreOffset.x,
-      y: fieldVerticalCenter - scoreOffset.y,
-    },
-  });
-  stage.addChild(secondaryPlayerScore);
-
-  // active player, score is below
-  const primaryPlayerScore = new TextComponent('1', {
-    ...scoreStyles,
-    position: {
-      x: scoreOffset.x,
-      y: fieldVerticalCenter + scoreOffset.y,
-    },
-  });
-  stage.addChild(primaryPlayerScore);
-};
 /**
  * @param {Object}
  */
