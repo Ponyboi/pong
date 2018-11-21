0a4a1aa46d45abc8d8ceb76e908e243c69648b2c
aahvocado <dan.dan.makes.stuff@gmail.com>
2018-11-20 22:42:12 -0800

    Ball collision with walls

--- public/js/components/BallComponent.js
+++ public/js/components/BallComponent.js
@@ -1,9 +1,11 @@
 import {
   Point as PIXI_Point,
 } from 'pixi.js';
+import { Rectangle as Intersects_Rectangle } from 'yy-intersects';
 
 import GameComponent from 'components/GameComponent';
 import { BALL_SIZE } from 'constants/sizes';
+import { WALL_LINES } from 'constants/positions';
 
 class BallComponent extends GameComponent {
   /** @default */
@@ -33,6 +35,27 @@ class BallComponent extends GameComponent {
   update() {
     // set the view's position
     this.view.position = this.position;
+
+    /** @type {Intersects.Rectangle} */
+    const hitbox = new Intersects_Rectangle(this.view, {
+      width: this.size.width,
+      height: this.size.height,
+      center: this.position,
+      noRotate: true,
+    });
+
+    if (hitbox.collidesLine(WALL_LINES.TOP.p1, WALL_LINES.TOP.p2)) {
+      this.velocity.y = -1 * this.velocity.y;
+    };
+    if (hitbox.collidesLine(WALL_LINES.RIGHT.p1, WALL_LINES.RIGHT.p2)) {
+      this.velocity.x = -1 * this.velocity.x;
+    };
+    if (hitbox.collidesLine(WALL_LINES.BOTTOM.p1, WALL_LINES.BOTTOM.p2)) {
+      this.velocity.y = -1 * this.velocity.y;
+    };
+    if (hitbox.collidesLine(WALL_LINES.LEFT.p1, WALL_LINES.LEFT.p2)) {
+      this.velocity.x = -1 * this.velocity.x;
+    };
   }
 };
 
