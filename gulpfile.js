2453402b2d773b2267e8873bcdda16f38a60f78d
aahvocado <dan.dan.makes.stuff@gmail.com>
2018-11-20 17:48:18 -0800

    update gulp default to be more flexible

--- gulpfile.js
+++ gulpfile.js
@@ -3,27 +3,63 @@ var webpack = require('webpack-stream');
 var nodemon = require("gulp-nodemon");
 var webpackConfig = require('./webpack.config.js');
 
+const isDevEnv = process.env.NODE_ENV === 'development';
+
 // build configuration is different for the web and server
 gulp.task('compile-server', function() {
   return gulp.src('./server/server.js')
     .pipe(webpack(webpackConfig.serverConfig))
     .pipe(gulp.dest('build'));
 });
-gulp.task('compile-web', function() {
+gulp.task('compile-webapp', function() {
+  // if using the dev environment, set up the config to watch for changes and refresh
+  if (isDevEnv) {
+    webpackConfig.webConfig.watch = true;
+  };
+
   return gulp.src('./app.js')
     .pipe(webpack(webpackConfig.webConfig))
     .pipe(gulp.dest('build'));
 });
 
+// only run the local web app
+gulp.task("run-webapp", function(cb) {
+  cb();
+});
+
 // Starts node server
-gulp.task("server", function () {
+gulp.task("run-server", function() {
   nodemon({
     script: './build/server.js',
   });
 });
 
 // default
-gulp.task('development', gulp.series('compile-web', 'compile-server', 'server'));
-gulp.task('default',
-  gulp.series(process.env.NODE_ENV || 'development')
-);
+gulp.task('dev-webapp', gulp.series('compile-webapp', 'run-webapp'));
+gulp.task('dev-server', gulp.series('compile-server', 'run-server'));
+gulp.task('development', gulp.series('compile-webapp', 'compile-server', 'run-server'));
+
+/**
+ * different 'default' task depending on settings
+ */
+function getDefaultTask() {
+  // only testing the pong app
+  if (process.env.DEV_ENV === 'webapp') {
+    return gulp.series('dev-webapp');
+  }
+
+  // test with the socket server
+  if (process.env.DEV_ENV === 'server') {
+    return gulp.series('dev-server');
+  }
+
+  // production
+  if (process.env.NODE_ENV === 'production') {
+    return gulp.series('production');
+  };
+
+  // use dev otherwise
+  return gulp.series('development');
+};
+
+gulp.task('default', getDefaultTask());
