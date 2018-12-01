d2541e22abc3f86042feb155cea0dc0740e6f5b5
Unknown <muffinking3@gmail.com>
2018-12-01 03:48:45 -0500

    update

--- public/js/managers/socketManager.js
+++ public/js/managers/socketManager.js
@@ -3,6 +3,7 @@ import io from 'socket.io-client';
 /**
  * start a connection to the server
  */
+console.log(process.env.NODE_ENV);
 const serverUrl = (process.env.NODE_ENV === 'production') ? window.location.href + process.env.PORT : 'http://localhost:666';
 const socketManager = io(serverUrl, {
   reconnection: false,
