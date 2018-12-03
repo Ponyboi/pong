258df7ac6955e0f23371d28aff83f3649b1a5419
Unknown <muffinking3@gmail.com>
2018-12-03 18:33:17 -0500

    adding protocol flex

--- public/js/managers/socketManager.js
+++ public/js/managers/socketManager.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 /**
  * start a connection to the server
  */
-const serverUrl = (!window.location.href.includes('localhost')) ? 'http://' + window.location.host : 'http://localhost:666';
+const serverUrl = (!window.location.href.includes('localhost')) ? window.location.protocol + '//' + window.location.host : 'http://localhost:666';
 const socketManager = io(serverUrl, {
   reconnection: false,
 });
