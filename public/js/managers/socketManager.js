177ca65497992b04bf0f758d1ab010f9b19355ae
Unknown <muffinking3@gmail.com>
2018-12-03 18:09:54 -0500

    changing socket URL

--- public/js/managers/socketManager.js
+++ public/js/managers/socketManager.js
@@ -4,7 +4,7 @@ import io from 'socket.io-client';
  * start a connection to the server
  */
 const serverUrl = (process.env.NODE_ENV === 'production') ? window.location.href + process.env.PORT : 'http://localhost:666';
-const serverUrl = (!window.location.href.includes('localhost:666')) ? window.location.href : 'http://localhost:666';
+const serverUrl = (!window.location.href.includes('localhost')) ? 'http://' + window.location.host : 'http://localhost:666';
 const socketManager = io(serverUrl, {
   reconnection: false,
 });
