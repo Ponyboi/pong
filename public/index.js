1815bda25802abdf19296eb5a934738482b823f5
Daniel Xiao <dan.dan.makes.stuff@gmail.com>
2018-12-03 19:30:49 -0800

    Pause (#16)
    * move game events from gameManager to eventManager
    
    * add pause button and pause state (not working yet)

--- public/index.js
+++ public/index.js
@@ -1,8 +1,12 @@
 /**
  * entry for the web app used by the client
+ *
+ * start by initializing all our managers
  */
 import 'managers/eventManager';
+import 'managers/gameManager';
 import 'managers/inputManager';
 import 'managers/pixiManager';
+import 'managers/socketManager';
 
 import './styles/main.css';
